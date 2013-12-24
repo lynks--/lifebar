1129ab7a9073a063c0af2c0fe23b8b91253c9fc1
Lynks <lynks@adranos.com>
2013-12-24 19:28:12 +0000

    updating licence

--- lifebar.h
+++ lifebar.h
@@ -4,9 +4,8 @@
  * Copyright (C) 2013 lynks@adranos.com
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License v3 as published
+ * by the Free Software Foundation.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,8 +14,6 @@
  * 
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * acpi.c is based heavily on the work of joey@kitenet.net
  *
 */
 
